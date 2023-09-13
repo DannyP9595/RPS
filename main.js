4569b5104728c696522b93b70e250d29e797c077
Danny Perez <Oscarzzy555@gmail.com>
2023-09-13 12:07:10 -0600

    Funcion para  que la computadora escoja entre R,P,S

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-function getComputerChoice(playerChoice, computerChoice) {
-    var S = "Scissor"
-    var P = "Paper"
-    var R = "Rock"
+function getComputerChoice{
+    //Generar un numero aleatorio entre 0 y 2
+    const numeroAleatorio = Math.floor(Math.random() * 3);
 
+    //Definir opciones con un Array
+    const options = ["Rock", "Paper", "Scissors"];
+
+    //Obtener la opcion al azar basada en el numero generado
+    const jugadaAlAzar = options[numeroAleatorio];
+
+    return jugadaAlAzar;
 
 }
+
+const jugada = getComputerChoice();
+console.log("La jugada es: ", jugada)
